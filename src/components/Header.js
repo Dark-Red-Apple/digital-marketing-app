2c4d9f2bf7665156d8ea6dd349a95b094357665e
Ali Jahangir <iraniandeveloper.net@gmail.com>
2023-04-16 11:43:07 -0300

    Edith menu in mobile size

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
-import { useState } from "react"
+import { useState } from "react";
 // import { Container } from "react-bootstrap"
-import NavAnimation from "./NavAnimation"
-import { Container, Row, Col } from "react-bootstrap"
-import servicesTechnology from "../assets/images/services-technology.jpg"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { icon } from "@fortawesome/fontawesome-svg-core"
+import NavAnimation from "./NavAnimation";
+import { Container, Row, Col } from "react-bootstrap";
+import servicesTechnology from "../assets/images/services-technology.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { icon } from "@fortawesome/fontawesome-svg-core";
 
 export default function Header() {
   return (
@@ -14,7 +14,10 @@ export default function Header() {
           <Col xs={6} sm={5} md={4} lg={4}>
             <div className="dma-header__logo dma-btn dma-btn--yellow">
               <i></i>
-              <FontAwesomeIcon icon={icon({ name: "line-chart" })} style={{ marginRight: "5px" }} />
+              <FontAwesomeIcon
+                icon={icon({ name: "line-chart" })}
+                style={{ marginRight: "5px" }}
+              />
               <a href="./">Digital Trend</a>
             </div>
           </Col>
@@ -31,12 +34,19 @@ export default function Header() {
                   <a href="/about-us">About us</a>
                 </li>
                 <li className="dma-btn ">
-                  <a className="dma-btn__yellow" href="/contact-us">Contact</a>
+                  <a className="dma-btn__yellow" href="/contact-us">
+                    Contact
+                  </a>
                 </li>
               </nav>
             </div>
           </Col>
-          <Col xs={4} sm={6} md={6} className="d-lg-none position-relative dma-header-mobile__menu">
+          <Col
+            xs={4}
+            sm={6}
+            md={6}
+            className="d-lg-none position-relative dma-header-mobile__menu"
+          >
             <NavAnimation>
               <nav className="dma-nav-links">
                 <li className="dma-btn dma-btn--yellow">
@@ -48,10 +58,8 @@ export default function Header() {
                 <li className="dma-btn dma-btn--yellow">
                   <a href="/about-us">About us</a>
                 </li>
-                <li className="dma-btn dma-btn__yellow">
-                  <a href="/contact-us" className="">
-                    Contact
-                  </a>
+                <li className="dma-btn dma-btn--yellow">
+                  <a href="/contact-us">Contact</a>
                 </li>
               </nav>
               <div className="dma-header-mobile__menu__promotion-images">
@@ -62,5 +70,5 @@ export default function Header() {
         </Row>
       </Container>
     </header>
-  )
+  );
 }
