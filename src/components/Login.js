c7422542999bac7e7a39dc6d95812d33c95f5c23
Ali Jahangir <iraniandeveloper.net@gmail.com>
2023-04-21 17:44:23 -0300

    Add Login Required validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,54 @@
-import React from "react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  function validation(e) {
+    e.preventDefault();
+    if (email === null || email === "") {
+      window.alert("please fill your Email!");
+    }
+    if (password === null || password === "") {
+      window.alert("please fill your Password!");
+    }
+  }
+
   return (
     <div className="dma-login text-black text-center  w-100 ">
       <h3>Please Login</h3>
       <form className="dma-login-form">
         <div className="dma-form-group mt-4 d-flex">
-          <input type="email" placeholder="Email" />
+          <input
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            type="email"
+            placeholder="Email"
+          />
         </div>
         <div className="dma-form-group mt-4 d-flex">
-          <input type="password" placeholder="Password" />
+          <input
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            placeholder="Password"
+          />
         </div>
-        <button className="mt-4 dma-btn dma-btn__bg-yellow">Login</button>
+        <button
+          onClick={validation}
+          className="mt-4 dma-btn dma-btn__bg-yellow"
+        >
+          Login
+        </button>
       </form>
-      <p className="mt-4">Dont have an account? <NavLink className="dma-sec-color-link dma-link" to={'/blog/register'}>Create one</NavLink> </p>
+      <p className="mt-4">
+        Dont have an account?{" "}
+        <NavLink className="dma-sec-color-link dma-link" to={"/blog/register"}>
+          Create one
+        </NavLink>{" "}
+      </p>
     </div>
   );
 }
