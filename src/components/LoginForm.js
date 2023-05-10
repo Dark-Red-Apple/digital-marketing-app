a5089da210fcb78e787fe708ae4a77a2e9fc84ea
Ali Jahangir <iraniandeveloper.net@gmail.com>
2023-05-10 13:17:22 -0300

    update Register validation with react-hook-form library

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,7 @@
-import { useState } from "react"
-import { useImmerReducer } from "use-immer"
-import { CSSTransition } from "react-transition-group"
-import { validEmail } from "./utility/Regex"
+import { useState } from "react";
+import { useImmerReducer } from "use-immer";
+import { CSSTransition } from "react-transition-group";
+import { validEmail } from "./utility/Regex";
 
 function LoginForm() {
   // const [email, setEmail] = useState(null);
@@ -31,80 +31,84 @@ function LoginForm() {
       message: "",
     },
     count: 0,
-  }
+  };
 
   function reducFunction(draft, action) {
     switch (action.type) {
       case "emailRequired": {
-        console.log(action.value == "" || action.value == null)
+        console.log(action.value == "" || action.value == null);
         if (action.value == "" || action.value == null) {
-          draft.email.error = true
-          draft.email.message = "Please enter your email!"
+          draft.email.error = true;
+          draft.email.message = "Please enter your email!";
         } else {
-          draft.email.error = false
+          draft.email.error = false;
           // draft.email.message = ""
         }
-        return
+        return;
       }
 
       case "validEmail": {
-        if (action.value != "" && action.value != null && !validEmail.test(action.value)) {
-          draft.email.error = true
-          draft.email.message = "Please enter a valid email!"
+        if (
+          action.value != "" &&
+          action.value != null &&
+          !validEmail.test(action.value)
+        ) {
+          draft.email.error = true;
+          draft.email.message = "Please enter a valid email!";
         } else if (action.value != "" && action.value != null) {
-          draft.email.error = false
+          draft.email.error = false;
         }
-        return
+        return;
       }
       case "passwordRequired": {
         if (action.value == "" || action.value == null) {
-          draft.password.message = "Please enter your password!"
-          draft.password.error = true
+          draft.password.message = "Please enter your password!";
+          draft.password.error = true;
         } else {
-          draft.password.error = false
+          draft.password.error = false;
           // draft.password.message = ""
         }
-        return
+        return;
       }
       case "validationLogin": {
         if (draft.email.error || draft.password.error) {
-          draft.summerError.message = "please enter  all required inputs"
+          draft.summerError.message = "please enter  all required inputs";
         } else {
-          draft.summerError.message = ""
-          draft.summerError.count++
+          draft.summerError.message = "";
+          draft.summerError.count++;
         }
 
-        return
+        return;
       }
     }
   }
 
-  const [state, dispatch] = useImmerReducer(reducFunction, initialState)
+  const [state, dispatch] = useImmerReducer(reducFunction, initialState);
 
   function validate(e) {
-    e.preventDefault()
-    console.log(e.target[0].value)
+    e.preventDefault();
+    console.log(e.target[0].value);
 
-    dispatch({ type: "emailRequired", value: e.target[0].value })
-    dispatch({ type: "validEmail", value: e.target[0].value })
-    dispatch({ type: "passwordRequired", value: e.target[1].value })
+    dispatch({ type: "emailRequired", value: e.target[0].value });
+    dispatch({ type: "validEmail", value: e.target[0].value });
+    dispatch({ type: "passwordRequired", value: e.target[1].value });
     // console.log(e.target[1]);
     // dispatch({ type: "validationLogin" });
     // console.log("ali");
     // window.alert("ali ");
   }
   function onKeyDownEmail(e) {
-    if (e.key === "Tab") {
-      // e.preventDefault()
-      dispatch({ type: "emailRequired", value: e.target.value })
-      dispatch({ type: "validEmail", value: e.target.value })
-    }
+    // if (e.key === "Tab") {
+    //   // e.preventDefault()
+    dispatch({ type: "emailRequired", value: e.target.value });
+    dispatch({ type: "validEmail", value: e.target.value });
+    // }
   }
   function onKeyDownPassword(e) {
-    if (e.key === "Tab") {
-      // e.preventDefault()
-      dispatch({ type: "passwordRequired", value: e.target.value })
-    }
+    // if (e.key === "Tab") {
+    // e.preventDefault()
+    dispatch({ type: "passwordRequired", value: e.target.value });
+    // }
   }
   return (
     <div className="dma-login text-black text-center  w-100 ">
@@ -116,12 +120,19 @@ function LoginForm() {
             //   dispatch({ type: "emailRequired", value: e.target.value })
             // }}
             tabIndex="1"
-            onKeyDown={onKeyDownEmail}
+            onBlur={onKeyDownEmail}
             type="text"
             placeholder="Email"
           />
-          <CSSTransition in={state.email.error} classNames="formErrorMessage" timeout={330} unmountOnExit>
-            <div className="alert alert-danger small formErrorMessage">{state.email.message}</div>
+          <CSSTransition
+            in={state.email.error}
+            classNames="formErrorMessage"
+            timeout={330}
+            unmountOnExit
+          >
+            <div className="alert alert-danger small formErrorMessage">
+              {state.email.message}
+            </div>
           </CSSTransition>
         </div>
         <div className="dma-form-group mt-md-4 d-flex flex-column">
@@ -130,21 +141,32 @@ function LoginForm() {
             // onChange={(e) => {
             //   dispatch({ type: "passwordRequired", value: e.target.value })
             // }}
-            onKeyDown={onKeyDownPassword}
+            onBlur={onKeyDownPassword}
             type="password"
             placeholder="Password"
           />
-          <CSSTransition in={state.password.error} classNames="formErrorMessage" timeout={330} unmountOnExit>
-            <div className="alert alert-danger small formErrorMessage">{state.password.message}</div>
+          <CSSTransition
+            in={state.password.error}
+            classNames="formErrorMessage"
+            timeout={330}
+            unmountOnExit
+          >
+            <div className="alert alert-danger small formErrorMessage">
+              {state.password.message}
+            </div>
           </CSSTransition>
         </div>
 
-        <button tabIndex="3" type="submit" className="mt-4 dma-btn dma-btn__bg-yellow">
+        <button
+          tabIndex="3"
+          type="submit"
+          className="mt-4 dma-btn dma-btn__bg-yellow"
+        >
           Login
         </button>
         {/* <p>{state.summerError.message}</p> */}
       </form>
     </div>
-  )
+  );
 }
-export default LoginForm
+export default LoginForm;
--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,15 +1,18 @@
-import { useEffect, useRef } from "react"
-import { useForm } from "react-hook-form"
-import { CSSTransition } from "react-transition-group"
+import { useEffect, useRef } from "react";
+import { useForm } from "react-hook-form";
+import { CSSTransition } from "react-transition-group";
 
 function RegisterForm() {
-  const emailErrorRef = useRef(null)
-  const usernameErrorRef = useRef(null)
+  const emailErrorRef = useRef(null);
+  const usernameErrorRef = useRef(null);
+  const passwordErrorRef = useRef(null);
+  const passwordMatchErrorRef = useRef(null);
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({ mode: "onBlur", reValidateMode: false })
+    watch,
+  } = useForm({ mode: "all", reValidateMode: false });
 
   // Validate Inputs
   const email = register("email", {
@@ -18,7 +21,7 @@ function RegisterForm() {
       value: /^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$/,
       message: "Please Enter a Valid Email Address",
     },
-  })
+  });
 
   const userName = register("userName", {
     required: "Username is required",
@@ -37,7 +40,54 @@ function RegisterForm() {
        \n 2. can not contain _., ._, __, .. 
        \n 3. no _ at the beginning or the end`,
     },
-  })
+  });
+
+  const password = register("password", {
+    required: "Password is required",
+    // minLength: {
+    //   value: 8,
+    //   message: "The Password should be at least 6 characters",
+    // },
+    // maxLength: {
+    //   value: 20,
+    //   message: "The Password should be at most 20 characters",
+    // },
+    pattern: {
+      value:
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/,
+      message: `The Password must follow these rules:
+      At least one uppercase letter,
+       one lowercase letter,
+        one number and one special character
+        at least 8 characters
+        at most 20 characters
+        `,
+    },
+  });
+  const passwordMatch = register("passwordMatch", {
+    required: "passwordMatch is required",
+    // minLength: {
+    //   value: 8,
+    //   message: "The passwordMatch should be at least 6 characters",
+    // },
+    // maxLength: {
+    //   value: 20,
+    //   message: "The passwordMatch should be at most 20 characters",
+    // },
+    // pattern: {
+    //   value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]$/,
+    //   message: `The passwordMatch must follow these rules:
+    //   At least one uppercase letter,
+    //    one lowercase letter,
+    //     one number and one special character`,
+    // },
+
+    validate: (val) => {
+      if (watch("password") != val) {
+        return "Your passwords do no match";
+      }
+    },
+  });
 
   return (
     <div className="dma-register text-black text-center  w-100 ">
@@ -46,32 +96,90 @@ function RegisterForm() {
       <form
         className="dma-register-form"
         onSubmit={handleSubmit((data) => {
-          console.log(data)
+          console.log(data);
         })}
       >
         <div className="dma-form-group mt-md-4 d-flex flex-column">
-          <input {...email} onBlur={(e) => email.onBlur(e)} placeholder="Email" />
+          <input
+            {...email}
+            onBlur={(e) => email.onBlur(e)}
+            placeholder="Email"
+          />
           {/* the nodeRef is necessary or we will have findDOMNode is deprecated error */}
-          <CSSTransition in={Boolean(errors.email)} nodeRef={emailErrorRef} classNames="formErrorMessage" timeout={330} unmountOnExit>
-            <div className="alert alert-danger small formErrorMessage">{errors.email?.message}</div>
+          <CSSTransition
+            in={Boolean(errors.email)}
+            nodeRef={emailErrorRef}
+            classNames="formErrorMessage"
+            timeout={330}
+            unmountOnExit
+          >
+            <div className="alert alert-danger small formErrorMessage">
+              {errors.email?.message}
+            </div>
           </CSSTransition>
         </div>
         <div className="dma-form-group mt-md-4 d-flex flex-column">
-          <input {...userName} onBlur={(e) => userName.onBlur(e)} placeholder="User Name" />
-          {errors.userName && console.log(errors.userName)}
-          <CSSTransition in={Boolean(errors.userName)} nodeRef={usernameErrorRef} classNames="formErrorMessage" timeout={330} unmountOnExit>
-            <div className="alert alert-danger small formErrorMessage">{errors.userName?.message}</div>
+          <input
+            {...userName}
+            onBlur={(e) => userName.onBlur(e)}
+            placeholder="User Name"
+          />
+
+          <CSSTransition
+            in={Boolean(errors.userName)}
+            nodeRef={usernameErrorRef}
+            classNames="formErrorMessage"
+            timeout={330}
+            unmountOnExit
+          >
+            <div className="alert alert-danger small formErrorMessage">
+              {errors.userName?.message}
+            </div>
           </CSSTransition>
         </div>
         <div className="dma-form-group mt-md-4 d-flex flex-column">
-          <input type="password" placeholder="Password" />
+          <input
+            {...password}
+            // onBlur={(e) => password.onBlur(e)}
+            onChange={(e) => password.onChange(e)}
+            type="password"
+            placeholder="Password"
+          />
+          <CSSTransition
+            in={Boolean(errors.password)}
+            nodeRef={passwordErrorRef}
+            classNames="formErrorMessage"
+            timeout={330}
+            unmountOnExit
+          >
+            <div className="alert alert-danger small formErrorMessage">
+              {errors.password?.message}
+            </div>
+          </CSSTransition>
         </div>
         <div className="dma-form-group mt-md-4 d-flex flex-column">
-          <input type="password" placeholder="Password" />
+          <input
+            {...passwordMatch}
+            // onBlur={(e) => passwordMatch.onBlur(e)}
+            onChange={(e) => passwordMatch.onChange(e)}
+            type="password"
+            placeholder="Confirm Password"
+          />
+          <CSSTransition
+            in={Boolean(errors.passwordMatch)}
+            nodeRef={passwordMatchErrorRef}
+            classNames="formErrorMessage"
+            timeout={330}
+            unmountOnExit
+          >
+            <div className="alert alert-danger small formErrorMessage">
+              {errors.passwordMatch?.message}
+            </div>
+          </CSSTransition>
         </div>
         <input type="submit" className="mt-4 dma-btn dma-btn__bg-yellow" />
       </form>
     </div>
-  )
+  );
 }
-export default RegisterForm
+export default RegisterForm;
